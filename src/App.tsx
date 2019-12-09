eebf33b93c5602aa47c45ad46b68900195aac20d
krasovsky <vlad_krasovsky@yahoo.com>
2019-12-08 23:51:41 -0800

    css reflection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+
+import EarthImage from "./assets/earth.png";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      className="reflection"
+      style={{ backgroundImage: `url(${EarthImage})`, color: "red" }}
+    ></div>
   );
-}
+};
 
 export default App;
